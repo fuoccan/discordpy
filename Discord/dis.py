30bc61b3e710d8af7361a97752cdc1f071aff00a
fuoccan <133734659+fuoccan@users.noreply.github.com>
2023-06-29 01:25:42 +0000

    read message

---
+++ Discord/dis.py
@@ -0,0 +1,24 @@
+import discord
+import asyncio
+
+TOKEN = 'YOUR_DISCORD_TOKEN'
+CHANNEL_ID = 123456789 # ID của channel Discord bạn muốn gửi tin nhắn vào
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+    channel = client.get_channel(CHANNEL_ID)
+    while True:
+        await channel.send('hello')
+        await asyncio.sleep(2) # Đợi 2 giây trước khi gửi tin nhắn tiếp theo
+
+async def on_message(message):
+    if message.channel.id == CHANNEL_ID:
+        if 'aaaaaaaaaa' in message.content:
+            print('Tin nhắn chứa chuỗi "aaaaaaaaaa".')
+
+client.run(TOKEN)
+
